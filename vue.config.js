9e1824c9e3c67c473a96cd8d1c7e2e7a34de3cd7
kontimeridian <aishah@kontinentalist.com>
2021-07-31 15:58:34 +0800

    docs change route

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  publicPath:
+  process.env.NODE_ENV === "production"
+    ? "/sg-masjid-project"
+    : "/",
   transpileDependencies: [
-    'vuetify'
-  ]
-}
+    'vuetify',
+  ],
+};
